2207c52333f7e00fedbb1aea8aa690cea3f8fe80
희망 <122812652+w1shope@users.noreply.github.com>
2023-11-30 06:06:50 +0900

    앱 실행 아이콘 변경

--- MatchingTeam/app/src/main/java/com/example/matchingteam/activity/MainActivity.kt
+++ MatchingTeam/app/src/main/java/com/example/matchingteam/activity/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.matchingteam.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.matchingteam.activity.login.LoginActivity
-import com.example.matchingteam.activity.register.RegisterActivity
 import com.example.matchingteam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,17 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val registerBtn: Button = binding.buttonOpenRegisterForm
-        registerBtn.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-        }
-
-        val loginBtn: Button = binding.buttonOpenLoginForm
-        loginBtn.setOnClickListener {
-            val intent: Intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
